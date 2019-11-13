72cfa724175497269f08f2e83ed94b2adab9d87d
arran hemish <arranhemish@arrans-mbp.home>
2019-11-13 17:19:47 +0000

    fix name

--- App/app/main.py
+++ App/app/main.py
@@ -226,6 +226,7 @@ def get_recipe():
     image_id = request.args['image_id']
     image_results = Image.query.filter_by(id=image_id).first()
     # db.session.commit()
+    print(image_results)
     return jsonify({'status': image_results})
     
 
